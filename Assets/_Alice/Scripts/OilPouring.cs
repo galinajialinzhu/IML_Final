efaa441f9adcd64a09ddc0c0a1482b4ebd545536
uxalice <yli40@inside.artcenter.edu>
2023-10-16 23:55:45 -0700

    Stage 6 - Update Oil Pouring Effect
    Create the effect when oil fill the pot
    
    ”OilPouring“ for handling the pouring logic and ”OilFillPot“ for increasing the level of the oil in the pot.

--- Assets/_Alice/Scripts/OilPouring.cs
+++ Assets/_Alice/Scripts/OilPouring.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class OilPouring : MonoBehaviour
 {
     public Transform oilContainer;
     public Transform spoutTracker;  // Reference to the SpoutTracker GameObject

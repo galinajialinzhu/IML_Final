645092301d09a271b5636fe6030f7eac46cb8507
Galina <Jzhu9@inside.artcenter.edu>
2023-11-29 10:34:55 -0800

    gailna file update

--- Assets/_Galina/instruction/Huatuo.cs
+++ Assets/_Galina/instruction/Huatuo.cs
@@ -1,4 +1,8 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
 
 public class Huatuo : MonoBehaviour
 {
@@ -12,11 +16,11 @@ public class Huatuo : MonoBehaviour
     public AudioClip audioClip3;
     public AudioSource audioSource;
 
+    private bool hasSceneChanged = false;
+
     private void Start()
     {
         // Initial setup
-        //animator = GetComponent<Animator>();
-        //audioSource = GetComponent<AudioSource>();
         SetHuatuoState(1);
     }
 
@@ -46,6 +50,17 @@ public class Huatuo : MonoBehaviour
                 SetHuatuoState(3);
             }
         }
+
+        if (state == 3 && !audioSource.isPlaying && !hasSceneChanged)
+        {
+            // Play the audio clip for state 3
+            audioSource.clip = audioClip3;
+            audioSource.Play();
+            hasSceneChanged = true; // Mark that the scene has changed
+
+            // Perform a fade-out transition before changing the scene
+            StartCoroutine(FadeOutAndChangeScene("00 Home", audioClip3.length));
+        }
     }
 
     private void SetHuatuoState(int newState)
@@ -64,11 +79,29 @@ public class Huatuo : MonoBehaviour
                 // Audio clip 2 will be handled in the Update method
                 break;
             case 3:
-                audioSource.clip = audioClip3;
-                audioSource.Play(); // Play the audio clip for state 3
+                // Audio clip 3 will be handled in the Update method
                 break;
             default:
                 break;
         }
+    }
+
+    private IEnumerator FadeOutAndChangeScene(string sceneName, float delay)
+    {
+        Image fadeImage = GetComponentInChildren<Image>(); // Assuming the Image component is a child of the Canvas
+        Color startColor = fadeImage.color;
+        startColor.a = 0f; // Set initial alpha to 0
+
+        float elapsedTime = 0f;
+        while (elapsedTime < delay)
+        {
+            float alpha = Mathf.Lerp(0f, 1f, elapsedTime / delay);
+            fadeImage.color = new Color(startColor.r, startColor.g, startColor.b, alpha);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // Change the scene after the fade-out
+        SceneManager.LoadScene(sceneName);
     }
 }
